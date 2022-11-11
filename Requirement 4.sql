1cd7ea16d88377af782da47700ea64ad5cf8c2c7
jkroening507 <80649062+jkroening507@users.noreply.github.com>
2022-11-10 19:25:00 -0600

    Add files via upload

---
+++ Requirement 4.sql
@@ -0,0 +1,23 @@
+###########################Triggers#####################################################
+#This trigger checks to make sure that there are negative prices being entered for the products
+delimiter //
+Create Trigger PriceCheck
+before insert on product for each row
+begin 
+if new.price <= 0 AND new.product_id = 1 then set new.price = 1.00;
+elseif new.price <= 0 AND new.product_id = 2 then set new.price = 2.00;
+elseif new.price <= 0 AND new.product_id = 3 then set new.price = 3.00;
+elseif new.price <= 0 AND new.product_id = 4 then set new.price = 4.00;
+elseif new.price <= 0 AND new.product_id = 5 then set new.price = 5.00;
+end if;
+end//
+
+#This trigger checks to make sure that any inserted quantities are not negative
+delimiter //
+Create Trigger ProductCheck
+before insert on Stock for each row
+begin 
+if new.quantity < 0 then set new.quantity = 0;
+end if;
+end//
+delimiter ;
