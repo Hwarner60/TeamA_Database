94915e1cf530d10f66128c11439ef0fc020721d3
Hwarner60 <37120756+Hwarner60@users.noreply.github.com>
2022-12-02 16:18:08 -0600

    Delete TeamAProjectQuery1.sql

--- TeamAProjectQuery1.sql
+++
@@ -1,8 +0,0 @@
-SELECT the_schedule.emp_id, employee.emp_name, store.store_id
-FROM the_schedule
-INNER JOIN employee
-ON the_schedule.emp_id = employee.emp_id
-INNER JOIN store
-ON the_schedule.store_id = store.store_id
-
-/* Query to find out Employee schedule*/
